8d7d63fc3bf96c4b7bdccee15ace39f3a7531ee3
Thomas Coates <tcoates@mines.edu>
2025-10-12 23:38:27 -0400

    feat: add Group Anagram  solution

--- 004_group_anagram/solution.py
+++ 004_group_anagram/solution.py
@@ -1,9 +1,12 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        output = []
-        for index_a, a in enumerate(nums):
-            for index_b, b in enumerate(nums):
-                if nums[index_a] + nums[index_b] == target and index_a != index_b:
-                    output.append(index_a)
-                    output.append(index_b)
-                    return output
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+        for s in strs:
+            count = [0] * 26 # a-z
+            
+            for c in s:
+                count[ord(c) - ord("a")] += 1 
+            
+            result[tuple(count)].append(s)
+        
+        return list(result.values())
